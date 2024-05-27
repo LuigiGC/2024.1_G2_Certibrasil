6636bceb2796a3e253b624ceaab4f32e50ece615
LUIGI FERNANDO GIANNINI CARDOSO <202304419108@alunos.ibmec.edu.br>
2024-05-27 14:56:38 -0300

    ex

--- lista/mapaInterativo/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ToDoItem, ToDoList
-
-admin.site.register(ToDoItem)
-admin.site.register(ToDoList)
--- lista/mapaInterativo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MapainterativoConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "mapaInterativo"
