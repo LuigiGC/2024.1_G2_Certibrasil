5a7868afbe72eb730ec4084cfabd6d04abd11df5
Luigi Fernando <luigifcard3@gmail.com>
2024-06-09 15:32:23 -0300

    at

--- certibrasil/mapa/views.py
+++ certibrasil/mapa/views.py
@@ -1,2 +1,11 @@
+from .models import Cliente,Endereco,Certificacao
+from django.shortcuts import render, get_object_or_404, redirect
 
+def login(request):
+    return render(request, '.\login.html')
 
+def home(request):
+    return render(request, './index.html')
+
+def cadastro(request):
+    return render(request, './cadastro.html')
